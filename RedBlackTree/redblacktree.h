8a855434fff882f7104d5a135a61608b219b0cc2
Zackary Murphy <zmurphy1125@gmail.com>
2016-07-06 13:54:35 -0500

    Filled out some of the red black tree operations
    Filled out most of the red black tree operations. Still need to do
    deletion and a proper destructor

--- RedBlackTree/redblacktree.h
+++ RedBlackTree/redblacktree.h
@@ -17,9 +17,11 @@ class Red_Black_Tree {
         void rebalance(RedBlackNode<k,v>* added_node);
             
     public:
+        Red_Black_Tree();
+        
         Red_Black_Tree(k root_key, v root_value);
         
-        Red_Black_Tree(RedBlackNode<k,v>* root);
+        Red_Black_Tree(RedBlackNode<k,v>* new_root);
         
         void insert(RedBlackNode<k,v>* new_node);
         
@@ -33,8 +35,144 @@ class Red_Black_Tree {
 };
 
 template <class k, class v>
+Red_Black_Tree<k,v>::Red_Black_Tree() {
+    root = nullptr;
+}
+
+template <class k, class v>
 Red_Black_Tree<k,v>::Red_Black_Tree(k root_key, v root_value) {
-    root = new RedBlackNode(root_key, root_value);
+    root = new RedBlackNode<k,v>(root_key, root_value);
+}
+
+template <class k, class v>
+Red_Black_Tree<k,v>::Red_Black_Tree(RedBlackNode<k,v>* new_root) {
+    root = new_root;
+}
+
+template <class k, class v>
+void Red_Black_Tree<k,v>::rotate_left(RedBlackNode<k,v>* about_node) {
+    RedBlackNode<k,v>* r_child = about_node->get_right();
+    about_node->set_right(r_child->get_left());
+    if (r_child->get_left() != nullptr) {
+        r_child->get_left()->set_parent(about_node);
+    }
+    if (about_node->get_parent() == nullptr) {
+        r_child->set_parent(nullptr);
+        root = r_child;
+    }
+    else {
+        if (about_node == (about_node->get_parent()->get_left())) {
+            about_node->get_parent()->set_left(r_child);
+            r_child->set_parent(about_node->get_parent());
+        }
+        else {
+            about_node->get_parent()->set_right(r_child);
+            r_child->set_parent(about_node->get_parent());
+        }
+        r_child->set_left(about_node);
+        about_node->set_parent(r_child);
+    }
+}
+
+template <class k, class v>
+void Red_Black_Tree<k,v>::rotate_right(RedBlackNode<k,v>* about_node) {
+    RedBlackNode<k,v>* l_child = about_node->get_left();
+    about_node->set_left(l_child->get_right());
+    if (l_child->get_right() != nullptr) {
+        l_child->get_right()->set_parent(about_node);
+    }
+    if (about_node->get_parent() == nullptr) {
+        l_child->set_parent(nullptr);
+        root = l_child;
+    }
+    else {
+        if (about_node == (about_node->get_parent()->get_right())) {
+            about_node->get_parent()->set_right(l_child);
+            l_child->set_parent(about_node->get_parent());
+        }
+        else {
+            about_node->get_parent()->set_left(l_child);
+            l_child->set_parent(about_node->get_parent());
+        }
+        l_child->set_right(about_node);
+        about_node->set_parent(l_child);
+    }
+}
+
+template <class k, class v>
+void Red_Black_Tree<k,v>::rebalance(RedBlackNode<k,v>* added_node) {
+    //Added node is the root
+    if (added_node->get_parent() == nullptr) {
+        added_node->set_color(RedBlackColor::BLACK);
+        return;
+    }
+    //Added node has a black parent, tree is still valid
+    if (added_node->get_parent()->get_color() == RedBlackColor::BLACK) {
+        return;
+    }
+    //Added node's parent and uncle is red, so recolor parent, uncle,and
+    //grandparent, then recurse on grandparent
+    if ((added_node->get_uncle() != nullptr) && (added_node->get_uncle()->get_color() == RedBlackColor::RED)) {
+        added_node->get_parent()->set_color(RedBlackColor::BLACK);
+        added_node->get_uncle()->set_color(RedBlackColor::BLACK);
+        added_node->get_grandparent()->set_color(RedBlackColor::RED);
+        rebalance(added_node->get_grandparent());
+        return;
+    }
+    //Nodes aren't linear
+    if ((added_node == (added_node->get_parent()->get_right())) && ((added_node->get_parent()) == (added_node->get_grandparent()->get_left()))) {
+        rotate_left(added_node->get_parent());
+        added_node = added_node->get_left();
+    }
+    else if ((added_node == (added_node->get_parent()->get_left())) && ((added_node->get_parent()) == (added_node->get_grandparent()->get_right()))) {
+        rotate_right(added_node->get_parent());
+        added_node = added_node->get_right();
+    }
+    //Nodes are linear
+    added_node->get_parent()->set_color(RedBlackColor::BLACK);
+    if (added_node->get_grandparent() != nullptr) {
+        added_node->get_grandparent()->set_color(RedBlackColor::RED);
+    }
+    if (added_node == (added_node->get_parent()->get_left())) {
+        rotate_right(added_node->get_grandparent());
+    }
+    else {
+        rotate_left(added_node->get_grandparent());
+    }
+}
+
+template <class k, class v>
+void Red_Black_Tree<k,v>::insert(RedBlackNode<k,v>* new_node) {
+    if (root = nullptr) {
+        root = new_node;
+    }
+    else {
+        root->insert(new_node);
+    }
+    rebalance(new_node);
+    root->set_color(RedBlackColor::BLACK);
+}
+
+template <class k, class v>
+void Red_Black_Tree<k,v>::insert(k key, v value) {
+    insert(new RedBlackNode<k,v>(key, value ));
+}
+
+template <class k, class v>
+v Red_Black_Tree<k,v>::get(k key) {
+    RedBlackNode<k,v>* search_node = root;
+    while (search_node != nullptr) {
+        if (search_node->get_key() == key) {
+            return search_node->get_value();
+        }
+        if (key < search_node->get_key()) {
+            search_node = search_node->get_left();
+        }
+        else {
+            search_node = search_node->get_right();
+        }
+    }
+    return nullptr;
 }
 
 
--- RedBlackTree/test.cpp
+++ RedBlackTree/test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "redblacktree.h"
 int main() {
-
+    
+    Red_Black_Tree<int, int> test_tree();
     return 0;
 }
 
